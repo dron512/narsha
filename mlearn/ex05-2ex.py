c1982a62a379f107c3c91de8e58721c3e05ce8cd
DESKTOP-CQH3EL9\parkm <parkmyounghoi@gmail.com>
2021-04-04 16:23:00 +0900

    20210404

---
+++ mlearn/ex05-2ex.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_validate,StratifiedKFold,GridSearchCV,RandomizedSearchCV
+from scipy.stats import uniform,randint
+# from myex01 import AA
+
+
+wine = pd.read_csv('https://bit.ly/wine-date')
+
+data = wine[['alcohol','sugar','pH']].to_numpy()
+target = wine['class'].to_numpy()
+
+from sklearn.model_selection import train_test_split
+train_input,test_input,train_target,test_target = train_test_split(data,target,\
+    test_size=0.2,random_state=42)
+
+sub_input,val_input,sub_target,val_target = train_test_split(train_input, train_target,\
+    test_size=0.2,random_state=42)
+
+print(sub_input.shape,val_input.shape)
+
+dt = DecisionTreeClassifier(random_state=42)
+dt.fit(sub_input,sub_target)
+
+print(dt.score(sub_input,sub_target))
+print(dt.score(val_input,val_target))
+
+'''## 교차 검증'''
+
+scores = cross_validate(dt,train_input,train_target)
+print(scores)
+
+print(np.mean(scores['test_score']))
+
+scores = cross_validate(dt,train_input,train_target,cv = StratifiedKFold())
+print(np.mean(scores['test_score']))
+
+splitter = StratifiedKFold(n_splits=10,shuffle=True,random_state=42)
+scores = cross_validate(dt,train_input,train_target,cv=splitter)
+print(np.mean(scores['test_score']))
+
+'''###하이퍼파라미터튜닝'''
+
+# aa = AA()
+# aa.doA()
+
+params = {'min_impurity_decrease':[0.0001,0.0002,0.0003,0.0004,0.0005]}
+
+gs = GridSearchCV(DecisionTreeClassifier(random_state=42),params,n_jobs=-1)
+
+gs.fit(train_input,train_target)
+
+dt = gs.best_estimator_
+print(dt.score(train_input,train_target))
+
+print(gs.best_params_)
+print(gs.cv_results_['mean_test_score'])
+
+best_index = np.argmax(gs.cv_results_['mean_test_score'])
+print(gs.cv_results_['params'][best_index])
+
+params = {'min_impurity_decrease':np.arange(0.0001,0.001,0.0001),
+        'max_depth':range(5,20,1),
+        'min_samples_split':range(2,100,10)
+        }
+
+gs = GridSearchCV(DecisionTreeClassifier(random_state=42),params,n_jobs=-1)
+gs.fit(train_input,train_target)
+
+print(gs.best_params_)
+print(np.max(gs.cv_results_['mean_test_score']))
+
+'''### 랜덤 서치'''
+rgen = randint(0,10)
+rgen.rvs(10)
+
+np.unique(rgen.rvs(1000),return_counts=True)
+ugen = uniform(0,1)
+
+params = {'min_impurity_decrease':uniform(0.0001,0.001),
+        'max_depth':randint(20,50),
+        'min_samples_split':randint(2,5),
+        'min_samples_leaf':randint(1,25)
+        }
+
+gs = RandomizedSearchCV(DecisionTreeClassifier(random_state=42),params,n_iter=100,n_jobs=-1,random_state=42)
+
+gs.fit(train_input,train_target)
+print(gs.best_params_)
+
+print(np.max(gs.cv_results_['mean_test_score']))
+
+dt = gs.best_estimator_
+print(dt.score(test_input,test_target))
---
+++ mlearn/myex01.py
@@ -0,0 +1,5 @@
+class AA:
+    def __init__(self):
+        pass
+    def doA(self):
+        print('doA')
